a4dfc2e00861d01546778db82ef98304968fa4d7
wda <vorobieva2005dasha@gmail.com>
2025-02-25 14:22:02 +0700

    add ApplicationInfo

--- app/src/main/java/com/example/miko/screens/ApplicationInfo.kt
+++ app/src/main/java/com/example/miko/screens/ApplicationInfo.kt
@@ -14,9 +14,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.miko.R
 import com.example.miko.ui.theme.four
 import com.example.miko.ui.theme.one
 import com.example.miko.ui.theme.zk
@@ -72,7 +74,7 @@ fun ApplicationInfo(
             )
 
             Text(
-                text = "blablabla",
+                text = stringResource(R.string.application_info),
                 modifier = Modifier
                     .padding(vertical = 10.dp),
                 fontFamily = zk,
