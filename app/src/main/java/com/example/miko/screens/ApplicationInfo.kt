4f653d0fbe42fc1da0fcee8f0797097ec840706b
wda <vorobieva2005dasha@gmail.com>
2025-02-25 14:20:37 +0700

    add ApplicationInfo

--- app/src/main/java/com/example/miko/screens/ApplicationInfo.kt
+++ app/src/main/java/com/example/miko/screens/ApplicationInfo.kt
@@ -1,13 +1,84 @@
 package com.example.miko.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.miko.ui.theme.four
+import com.example.miko.ui.theme.one
+import com.example.miko.ui.theme.zk
 
 @Composable
 fun ApplicationInfo(
     navController: NavHostController,
     applicationinfo: MutableState<Boolean>
 ){
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize(),
+        containerColor = one
+    ){ innerPadding ->
 
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState()),
+            verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.Start
+            ) {
+                TextButton(
+                    modifier = Modifier,
+                    onClick = {
+                        navController.navigate("start")
+                    }
+                ) {
+                    Text(
+                        text = "назад",
+                        modifier = Modifier
+                            .padding(vertical = 10.dp),
+                        fontFamily = zk,
+                        fontSize = 20.sp,
+                        color = four
+                    )
+                }
+            }
+
+            Text(
+                text = "О ПРИЛОЖЕНИИ",
+                modifier = Modifier
+                    .padding(vertical = 10.dp),
+                fontFamily = zk,
+                fontSize = 40.sp,
+                color = four
+            )
+
+            Text(
+                text = "blablabla",
+                modifier = Modifier
+                    .padding(vertical = 10.dp),
+                fontFamily = zk,
+                fontSize = 30.sp,
+                color = four
+            )
+        }
+    }
 }
