49adf817e5995697e59d9cd651758b1864f23416
wda <vorobieva2005dasha@gmail.com>
2025-02-27 15:57:26 +0700

    add Profile Dialog

--- app/src/main/java/com/example/miko/screens/Profile.kt
+++ app/src/main/java/com/example/miko/screens/Profile.kt
@@ -1,6 +1,5 @@
 package com.example.miko.screens
 
-import android.widget.Toast
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
@@ -9,12 +8,16 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -30,8 +33,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import androidx.navigation.NavHostController
 import com.example.miko.R
 import com.example.miko.ui.theme.four
@@ -40,6 +45,7 @@ import com.example.miko.ui.theme.three
 import com.example.miko.ui.theme.two
 import com.example.miko.ui.theme.zk
 
+@ExperimentalMaterial3Api
 @Composable
 fun Profile(
     navController: NavHostController,
@@ -47,6 +53,10 @@ fun Profile(
 ){
     val userName = remember { mutableStateOf("") }
     val userAdress = remember { mutableStateOf("") }
+
+    val openDialog = remember { mutableStateOf(false) }
+    val emailCode = remember { mutableStateOf("") }
+    val passwordUser = remember { mutableStateOf("") }
 
     Scaffold(
         modifier = Modifier
@@ -270,6 +280,7 @@ fun Profile(
                 TextButton(
                     modifier = Modifier,
                     onClick = {
+                        openDialog.value = true
                     }
                 ) {
                     Text(
@@ -279,6 +290,186 @@ fun Profile(
                         color = four,
                         fontSize = 24.sp
                     )
+                }
+
+                if (openDialog.value) {
+                    Dialog(
+                        onDismissRequest = { openDialog.value = false}
+                    ){
+                        Card(
+                            modifier = Modifier
+                                .size(300.dp, 400.dp),
+                            colors = CardDefaults.cardColors(one),
+                            shape = RoundedCornerShape(
+                                30.dp, 30.dp, 30.dp, 30.dp
+                            ),
+                            border = BorderStroke(
+                                width = 2.dp,
+                                color = four),
+                        ) {
+
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .padding(horizontal = 15.dp),
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                verticalArrangement = Arrangement.spacedBy(15.dp)
+                            ) {
+                                Column(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(top = 20.dp),
+                                    horizontalAlignment = Alignment.Start
+                                ) {
+                                    OutlinedTextField(
+                                        value = TextFieldValue("КОД НА ПОЧТУ"),
+                                        onValueChange = {},
+                                        modifier = Modifier,
+                                        shape = RoundedCornerShape(
+                                            30.dp, 30.dp, 30.dp, 0.dp
+                                        ),
+                                        enabled = false,
+                                        readOnly = true,
+                                        textStyle = TextStyle(
+                                            fontFamily = zk,
+                                            fontSize = 20.sp,
+                                            color = four
+                                        ),
+                                        colors = TextFieldDefaults.colors(
+                                            focusedIndicatorColor = four,
+                                            unfocusedIndicatorColor = four,
+                                            disabledContainerColor = two
+                                        )
+                                    )
+                                }
+
+                                Column(
+                                    modifier = Modifier
+                                        .fillMaxWidth(),
+                                    horizontalAlignment = Alignment.End
+                                ) {
+                                    OutlinedTextField(
+                                        value = emailCode.value,
+                                        onValueChange = {
+                                                new -> emailCode.value = new.uppercase()
+                                        },
+                                        modifier = Modifier,
+                                        shape = RoundedCornerShape(
+                                            30.dp, 30.dp, 0.dp, 30.dp
+                                        ),
+                                        enabled = true,
+                                        readOnly = false,
+                                        textStyle = TextStyle(
+                                            fontFamily = zk,
+                                            fontSize = 20.sp,
+                                            color = four
+                                        ),
+                                        placeholder = { Text("ВВЕСТИ КОД",
+                                            color = three,
+                                            fontFamily = zk,
+                                            fontSize = 20.sp,) },
+                                        colors = TextFieldDefaults.colors(
+                                            cursorColor = three,
+                                            focusedContainerColor = two,
+                                            unfocusedContainerColor = two,
+                                            focusedIndicatorColor = four,
+                                            unfocusedIndicatorColor = four
+                                        )
+                                    )
+                                }
+
+                                Column(
+                                    modifier = Modifier
+                                        .fillMaxWidth(),
+                                    horizontalAlignment = Alignment.Start
+                                ) {
+                                    OutlinedTextField(
+                                        value = "НОВЫЙ ПАРОЛЬ",
+                                        onValueChange = {},
+                                        modifier = Modifier,
+                                        shape = RoundedCornerShape(
+                                            30.dp, 30.dp, 30.dp, 0.dp
+                                        ),
+                                        enabled = false,
+                                        readOnly = true,
+                                        textStyle = TextStyle(
+                                            fontFamily = zk,
+                                            fontSize = 20.sp,
+                                            color = four
+                                        ),
+                                        colors = TextFieldDefaults.colors(
+                                            focusedIndicatorColor = four,
+                                            unfocusedIndicatorColor = four,
+                                            disabledContainerColor = two
+                                        )
+                                    )
+                                }
+
+                                Column(
+                                    modifier = Modifier
+                                        .fillMaxWidth(),
+                                    horizontalAlignment = Alignment.End
+                                ) {
+                                    OutlinedTextField(
+                                        value = passwordUser.value,
+                                        onValueChange = { new ->
+                                            passwordUser.value = new.uppercase()
+                                        },
+                                        modifier = Modifier,
+                                        shape = RoundedCornerShape(
+                                            30.dp, 30.dp, 0.dp, 30.dp
+                                        ),
+                                        enabled = true,
+                                        readOnly = false,
+                                        textStyle = TextStyle(
+                                            fontFamily = zk,
+                                            fontSize = 20.sp,
+                                            color = four
+                                        ),
+                                        placeholder = {
+                                            Text(
+                                                "ВВЕСТИ ПАРОЛЬ",
+                                                color = three,
+                                                fontFamily = zk,
+                                                fontSize = 20.sp,
+                                            )
+                                        },
+                                        colors = TextFieldDefaults.colors(
+                                            cursorColor = three,
+                                            focusedContainerColor = two,
+                                            unfocusedContainerColor = two,
+                                            focusedIndicatorColor = four,
+                                            unfocusedIndicatorColor = four
+                                        )
+                                    )
+                                }
+
+                                Button(
+                                    shape = RoundedCornerShape(
+                                        30.dp, 30.dp, 30.dp, 30.dp
+                                    ),
+                                    modifier = Modifier,
+                                    onClick = {
+                                        // добавить изменение данных в бд
+                                        openDialog.value = false
+                                    },
+                                    border = BorderStroke(
+                                        width = 2.dp,
+                                        color = four
+                                    ),
+                                    colors = ButtonDefaults.buttonColors(two)
+                                ) {
+                                    Text(
+                                        text = "ОК",
+                                        modifier = Modifier,
+                                        fontFamily = zk,
+                                        color = four,
+                                        fontSize = 24.sp
+                                    )
+                                }
+                            }
+                        }
+                    }
                 }
 
                 TextButton(
